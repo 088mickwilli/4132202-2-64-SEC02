a538dc4c0ad3a7a88afa9fc55639febb7e0d1d9b
macminlan14 <630112418088@bru.ac.th>
2021-12-16 16:42:56 +0700

    react

--- LAB2/es6-arry.js
+++ LAB2/es6-arry.js
@@ -25,3 +25,45 @@ const newArr2 = [...cars,...rname];
 console.log(newArr2);
 
 console.log(newArr.toString());
+
+//itteration
+let text = "";
+for(value of rname){
+    text += value + ":"
+}
+console.log(text);
+
+const numbers = [10,20,30]
+numbers.forEach(addNum);
+console.log(numbers);
+
+function addNum(value,index,array){
+    // value += 2;
+    console.log(value += 2);
+}
+
+const ns = numbers.map(addNumMap);
+console.log(ns);
+function addNumMap(value){
+    return value*2;
+}
+
+const over10 = numbers.filter(over10fn);
+console.log(over10);
+function over10fn(value){
+    return value > 10;
+}
+
+const reOver10 = numbers.reduce(over10fn)
+console.log(reOver10);
+
+const everOver10 = numbers.every(over10fn)
+console.log(everOver10);
+const someOver10 = numbers.some(over10fn)
+console.log(someOver10);
+const resFindIndex = numbers.indexOf(over10fn)
+console.log(resFindIndex);
+
+let alphab = Array.from("ABCDEFG021");
+console.log(alphab);
+
---
+++ lab3/src/components/Car.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const dom =(<>
+    <h1>Hello React..!</h1>
+    <p>My name is Mick</p>
+    </>
+);
+
+function Car(){
+
+    return(
+        <>{dom}</>
+    )}
+
+export default Car;
